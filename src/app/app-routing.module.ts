96a24c8b7811ae856f7d7a520a7a8da08cf5bf02
CaptainLomik <nastiy248@mail.ru>
2020-09-03 19:08:31 +0300

    start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { CartComponent } from './site/cart/cart.component';
+import { MainPageComponent } from './site/main-page/main-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:MainPageComponent},
+  {path:'cart', component:CartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './site/menu/menu.component';
+import { MainPageComponent } from './site/main-page/main-page.component';
+import { CartComponent } from './site/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MainPageComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
